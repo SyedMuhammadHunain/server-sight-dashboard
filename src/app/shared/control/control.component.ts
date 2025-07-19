fb0c1a144ca71c4a4f1b4df54e929536f6a00399
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-07-19 14:59:51 +0500

    added Host Elements, Encapsulation & Class binding (blinking status)

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -7,7 +7,14 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
+  onClick() {
+    console.log('hellow')
+  }
 }
