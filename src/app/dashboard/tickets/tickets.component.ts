ff6f5f2e6bfd0849fa9f2e58a5aec387abefc344
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-07-23 08:28:47 +0500

    added ticket form & show the available ticket

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './tickets.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  styleUrl: './tickets.component.css',
 })
 export class TicketsComponent {
+  tickets: Ticket[] = [];
 
+  onAdd(ticketData: { title: string; request: string }) {
+    const ticket: Ticket = {
+      id: Math.random().toString(),
+      title: ticketData.title,
+      request: ticketData.request,
+      status: 'open',
+    };
+    this.tickets.push(ticket);
+  }
 }
